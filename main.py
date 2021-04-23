7046e6c43f22c7ee3697944766fcec0672c7ea79
Vladislav <vladislav0904@gmail.com>
2021-04-24 03:14:43 +0500

    Small fix for heroku v2.0

--- main.py
+++ main.py
@@ -335,10 +335,11 @@ def signings():
 def main():
     db_session.global_init("db/estate.db")
     # db_sess = db_session.create_session()
+
     app.register_blueprint(blueprint)
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    main()
